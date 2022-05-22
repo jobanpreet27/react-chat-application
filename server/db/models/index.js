aa0a6a0f469e70173f474ad4309491962c702535
jobanpreet <joban52@gmail.com>
2022-05-21 23:46:41 -0500

    Implement many to many relationship between Users and Conversations

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,7 +1,7 @@
 const Conversation = require("./conversation");
 const User = require("./user");
 const Message = require("./message");
-
+const Group = require("./group");
 // associations
 
 User.hasMany(Conversation);
@@ -9,9 +9,12 @@ Conversation.belongsTo(User, { as: "user1" });
 Conversation.belongsTo(User, { as: "user2" });
 Message.belongsTo(Conversation);
 Conversation.hasMany(Message);
+User.belongsToMany(Conversation, { through: Group });
+Conversation.belongsToMany(User, { through: Group });
 
 module.exports = {
   User,
   Conversation,
-  Message
+  Message,
+  Group,
 };
